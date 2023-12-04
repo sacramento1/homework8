0e44f4e636561bb3ae7e66cd96d6047513b984db
sacramento1 <147737922+sacramento1@users.noreply.github.com>
2023-12-04 20:25:45 +0500

    imtixon

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import main
 
 urlpatterns = [
-    path('main/', main, name='main'),
+    path('post/', main, name='main'),
     # path('post/<str:pk>', detail, name='detail')
 ]
--- myproject/settings.py
+++ myproject/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'imtixon',
+        'USER': 'postgres',
+        'PASSWORD': 'samandar09',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
