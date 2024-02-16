0bc8681353fa687f821ddc0bba3946c55c5f585d
sacramento1 <147737922+sacramento1@users.noreply.github.com>
2024-02-16 14:44:58 +0500

    feat: change blog

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import main
 
 urlpatterns = [
-    path('post/', main, name='main'),
+    path('', main, name='main'),
     # path('post/<str:pk>', detail, name='detail')
 ]
